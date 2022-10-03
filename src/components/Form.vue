19bed7f8c23dd950d5fd08051ecde9fd08f2ca03
MaaviaAsghar4 <maavia.asghar.96@gmail.com>
2022-10-03 12:43:16 +0500

    Fix form styling

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,8 +1,8 @@
 <template>
   <form>
     <label>Enter Todo</label>
-    <input type="text" placeholder="Add Task" />
-    <input type="submit" />
+    <input id="type-text" type="text" placeholder="Add Task" />
+    <input id="type-submit" type="submit" />
   </form>
 </template>
 
@@ -16,20 +16,35 @@ export default {
 form {
   display: flex;
   flex-direction: column;
-  width: fit-content;
+  align-items: center;
 }
 
 label {
-  align-self: flex-start;
   font-size: 12px;
   color: #000;
   font-weight: 600;
   margin-bottom: 5px;
 }
 
-input {
+#type-submit {
+  border: none;
+  padding: 10px;
+  background-color: #2c3e50;
+  color: #f2f2f2;
+  cursor: pointer;
   margin-bottom: 10px;
-  width: 100%;
   max-width: 300px;
+  width: 100%;
+  border-radius: 5px;
+}
+
+#type-text {
+  border: 1px solid #2c3e50;
+  padding: 10px;
+  color: #2c3e50;
+  margin-bottom: 10px;
+  max-width: 300px;
+  width: 100%;
+  border-radius: 5px;
 }
 </style>
